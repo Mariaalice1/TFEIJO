1e097ad98402ec9dc333d4bd45a8f9bde3159c9c
Mariaalice1 <85592927+Mariaalice1@users.noreply.github.com>
2021-09-27 14:44:05 -0300

    atualizando1

---
+++ aprendendo.py
@@ -0,0 +1,7 @@
+nome = input("Digite o seu nome:")
+idade = int(input("Digite a sua idade por favor:"))
+altura = float(input("Digite a sua altura:"))
+peso = int(input("Digite o seu peso:"))
+
+print(nome, idade, altura, peso)
+print("------Parabens!!O seu cadastro foi concluido com sucesso.-------")
